30d1c04df235e1ae7fa840441f675f65fa1875cc
Robin <teachmetheways652@gmail.com>
2021-06-23 17:55:17 -0700

    Update
    Added Copyright

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 First Github, nothing special its just a hello world
+Testing out features
--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
+/*
+Copyright 2020 Robin Simpson (teachmetheways652@gmail.com)
+main.cpp (c) 2021
+Desc: Test
+Created:  2021-06-24T00:33:41.131Z
+Modified: !date!
+*/
 #include <iostream>
 
 int main() {
   std::cout << "Hello World!\n";
+
+  return 0;
 }
